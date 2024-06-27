0d711a4d7259175ac00167a22f40ca721087c2cc
adminMV <mihaylovvalera33@gmail.com>
2024-06-27 20:21:59 +0300

    Начал разработку EventProcessor

---
+++ events/telegram/telegram.go
@@ -0,0 +1,105 @@
+package telegram
+
+import (
+	"errors"
+
+	"TelegramBotGolang/clients/telegram"
+	"TelegramBotGolang/events"
+	"TelegramBotGolang/lib/e"
+	"TelegramBotGolang/storage"
+)
+
+type Processor struct {
+	tg      *telegram.Client
+	offset  int
+	storage storage.Storage
+}
+
+type Meta struct {
+	ChatID   int
+	Username string
+}
+
+var (
+	ErrUnknownEventType = errors.New("unknown event type")
+	ErrUnknownMetaType  = errors.New("unknown meta type")
+)
+
+func New(client *telegram.Client, storage storage.Storage) *Processor {
+	return &Processor{
+		tg:      client,
+		storage: storage,
+	}
+}
+
+func (p *Processor) Fetch(limit int) ([]events.Event, error) {
+	updates, err := p.tg.Updates(p.offset, limit)
+	if err != nil {
+		return nil, e.Wrap("can't get get events", err)
+	}
+
+	if len(updates) == 0 {
+		return nil, nil
+	}
+
+	res := make([]events.Event, 0, len(updates))
+
+	for _, u := range updates {
+		res = append(res, event(u))
+	}
+
+	p.offset = updates[len(updates)-1].ID + 1
+}
+
+func (p *Processor) processMessage(event events.Event) {
+	meta, err := meta(event)
+}
+
+func meta(event events.Event) (Meta, error) {
+	res, ok := event.Meta.(Meta)
+	if !ok {
+		return Meta{}, e.Wrap("can't get meta")
+	}
+}
+
+func event(upd telegram.Update) events.Event {
+	updType := fetchType(upd)
+
+	res := events.Event{
+		Type: updType,
+		Text: fetchText(upd),
+	}
+
+	if updType == events.Message {
+		res.Meta = Meta{
+			ChatID:   upd.Message.Chat.ID,
+			Username: upd.Message.From.Username,
+		}
+	}
+	return res
+}
+
+func fetchText(upd telegram.Update) string {
+	if upd.Message == nil {
+		return ""
+	}
+
+	return upd.Message.Text
+}
+
+func fetchType(upd telegram.Update) events.Type {
+	if upd.Message == nil {
+		return events.Unknown
+	}
+
+	return events.Message
+}
+
+func (p *Processor) Process(event events.Event) error {
+	switch event.Type {
+	case events.Message:
+		p.processMessage(event)
+	default:
+		return e.Wrap("can't proccess message", ErrUnknownEventType)
+	}
+}
